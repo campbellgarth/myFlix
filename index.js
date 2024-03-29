a89cf5a14b05e2223317eab0ed88a08674865eff
campbellgarth <157370612+campbellgarth@users.noreply.github.com>
2024-03-29 19:51:58 +0100

    testing login error

--- index.js
+++ index.js
@@ -18,18 +18,18 @@ const { check, validationResult } = require('express-validator');
 const cors = require('cors');
 let allowedOrigins = ['http://localhost:8080', 'https://myflixmovies-72c1f6d2bace.herokuapp.com'];
 //set CORS to only specific origins
-// app.use(cors({
-//     origin: (origin, callback) => {
-//         if (!origin) return callback(null, true);
-//         if (allowedOrigins.indexOf(origin) === -1) { //if origin isn't found in list of allowed origins
-//             let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
-//             return callback(new Error(message), false);
-//         }
-//         return callback(null, true);
-//     }
-// }));
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) { //if origin isn't found in list of allowed origins
+            let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
+            return callback(new Error(message), false);
+        }
+        return callback(null, true);
+    }
+}));
 
-app.use(cors()); //allows requests from all origins instead of above
+// app.use(cors()); //allows requests from all origins instead of above
 let auth = require('./auth')(app); //use auth.js code (app) argument allows express to be available in auth.js as well
 const passport = require('passport');
 require('./passport'); //imports passport.js
